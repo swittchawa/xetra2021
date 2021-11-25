5bc01fc9607d77d3c4bc04c1af079aec211b37f5
Switt Chawanwanidchai <swittchawanwanidchai@Switts-MacBook-Pro.local>
2021-11-25 15:26:28 +0700

    Entrypoint + pipenv implemented

--- run.py
+++ run.py
@@ -1,21 +1,51 @@
 """Running the Xetra ETL application"""
+import argparse
 import logging
 import logging.config
 
 import yaml
+
+from xetra.common.s3 import S3BucketConnector
+from xetra.transformers.xetra_transformer import XetraETL, XetraSourceConfig, XetraTargetConfig
 
 def main():
     """
         entry point to run the Xetra ETL job
     """
     # Parsing the YAML file
+    parser = argparse.ArgumentParser(description='Run the Xetra ETL job.')
     config_path = '/Users/swittchawanwanidchai/xetra_project/xetra2021/configs/xetra_report1_config.yml'
+    args = parser.parse_args()
     config = yaml.safe_load(open(config_path))
+
     # Configure logging
     log_config = config['logging']
     logging.config.dictConfig(log_config)
     logger = logging.getLogger(__name__)
-    logger.info("This is a test.")
+    # reading s3 configuration
+    s3_config = config['s3']
+    # Creating the S3BucketConnector class instances for source and target
+    s3_bucket_src = S3BucketConnector(access_key=s3_config['access_key'],
+                                        secrect_key=s3_config['secrect_key'],
+                                        endpoint_url=s3_config['src_endpoint_url'],
+                                        bucket=s3_config['src_bucket'])
+    s3_bucket_trg = S3BucketConnector(access_key=s3_config['access_key'],
+                                        secrect_key=s3_config['secrect_key'],
+                                        endpoint_url=s3_config['trg_endpoint_url'],
+                                        bucket=s3_config['trg_bucket'])
+    # Reading source configuration
+    source_config = XetraSourceConfig(**config['source'])
+    # Reading target configuration
+    target_config = XetraTargetConfig(**config['target'])
+    # Reading meta file configuration
+    meta_config = config['meta']
+    # Creating XetraETL class instance
+    logger.info('Xetra ETL job started')
+    xetra_etl = XetraETL(s3_bucket_src, s3_bucket_trg,
+                            meta_config['meta_key'], source_config, target_config)
+    # running ETL job for xetra report
+    xetra_etl.etl_report1()
+    logger.info('Xetra ETL job finished.')
 
 if __name__ == '__main__':
     main()
--- xetra/transformers/xetra_transformer.py
+++ xetra/transformers/xetra_transformer.py
@@ -1,6 +1,11 @@
 """Xetra ETL Component"""
 import logging
+from datetime import datetime
 from typing import NamedTuple
+
+import pandas as pd
+
+from xetra.common.meta_process import MetaProcess
 from xetra.common.s3 import S3BucketConnector
 
 class XetraSourceConfig(NamedTuple):
@@ -15,6 +20,7 @@ class XetraSourceConfig(NamedTuple):
     src_col_start_price: column name for starting price in source
     src_col_min_price: column name for minimum price in source
     src_col_max_price: column name for maximum in source
+    src_col_traded_vol: column name for traded volumne in source
     """
     src_first_extract_date: str
     src_columns: list
@@ -25,6 +31,7 @@ class XetraSourceConfig(NamedTuple):
     src_col_min_price: str
     src_col_max_price: str
     src_col_traded_vol: str
+
 
 class XetraTargetConfig(NamedTuple):
     """
@@ -59,7 +66,9 @@ class XetraETL():
     Reads the Xetra data, transforms and writes the transformed to target
     """
 
-    def __init__(self, s3_bucket_src: S3BucketConnector, s3_bucket_trg: S3BucketConnector, meta_key: str, src_args: XetraSourceConfig, trg_args: XetraSourceConfig):
+    def __init__(self, s3_bucket_src: S3BucketConnector, 
+                s3_bucket_trg: S3BucketConnector, meta_key: str, 
+                src_args: XetraSourceConfig, trg_args: XetraSourceConfig):
         """
         Constructor for XetraTransformer
 
@@ -75,18 +84,124 @@ class XetraETL():
         self.meta_key = meta_key
         self.src_args = src_args
         self.trg_args = trg_args
-        self.extract_date =
-        self.extract_date_list =
-        self.meta_update_list = 
+        self.extract_date, self.extract_date_list = MetaProcess.return_date_list(
+            self.src_args.src_first_extract_date, self.meta_key, self.s3_bucket_trg)
+        self.meta_update_list = [date for date in self.extract_date_list\
+            if date >= self.extract_date]
 
     def extract(self):
-        pass
+        """
+        Read the source data and concatenates them to one Pandas DataFrame
+
+        :returns:
+            data_frame: Pandas DataFrame with the extracted data
+        """
+        self._logger.info('Extracting Xetra source files started...')
+        files = [key for date in self.extract_date_list\
+                        for key in self.s3_bucket_src.list_files_in_prefix(date)]
+        if not files:
+            data_frame = pd.DataFrame()
+        else:
+            data_frame = pd.concat([self.s3_bucket_src.read_csv_to_df(file)\
+                for file in files], ignore_index=True)
+        self._logger.info('Extracting Xetra source files finished.')
+        return data_frame
 
     def transform_report1(self):
-        pass
+        """
+        Applies the necessary transformation to create report 1
 
-    def load(self):
-        pass
+        :param data_frame: Pandas DataFrame as Input
+
+        :returns:
+            data_frame: Transformed Pandas DataFrame as output
+        """
+        if data_frame.empty(self):
+            self._logger.info("The dataframe is empty. No transformation will be applied")
+            return data_frame
+        self._logger.info('Applying transformation to Xetra source data for report 1 start...')
+        # Filtering necessary source columns
+        data_frame = data_frame.loc[:, self.src_args.src_columns]
+        # Removing rows with missing values
+        data_frame.dropna(inplace=True)
+        # Calculating opening price per ISIN and day
+        data_frame[self.trg_args.trg_col_op_price] = data_frame\
+            .sort_values(by=[self.src_args.src_col_time])\
+                .groupby([
+                    self.src_args.src_col_isin,
+                    self.src_args.src_col_date
+                    ])[self.src_args.src_col_start_price]\
+                        .transform('first')
+        # Calculating closing price per ISIN and day
+        data_frame[self.trg_args.trg_col_clos_price] = data_frame\
+            .sort_values(by=[self.src_args.src_col_time])\
+                .groupby([
+                    self.src_args.src_col_isin,
+                    self.src_args.src_col_date
+                    ])[self.src_args.src_col_start_price]\
+                        .transform('last')
+        # Renaming columns
+        data_frame.rename(columns={
+            self.src_args.src_col_min_price: self.trg_args.trg_col_min_price,
+            self.src_args.src_col_max_price: self.trg_args.trg_col_max_price,
+            self.src_args.src_col_traded_vol: self.trg_args.trg_col_dail_trad_vol
+        }, inplace=True)
+        # Aggregating per ISIN and day -> openning price, closing price,
+        # minimum price, maximum price, traded volumn
+        data_frame = data_frame.groupby([
+            self.src_args.src_col_isin,
+            self.src_args.src_col_date], as_index=False)\
+                .agg({
+                    self.trg_args.trg_col_op_price: 'min',
+                    self.trg_args.trg_col_clos_price: 'min',
+                    self.trg_args.trg_col_min_price: 'min',
+                    self.trg_args.trg_col_max_price: 'max',
+                    self.trg_args.trg_col_dail_trad_vol: 'sum'})
+        # Change of current day's closing price compared to the
+        # previous traiding day's closing price in %
+        data_frame[self.trg_args.trg_col_ch_prev_clos] = data_frame\
+            .sort_values(by=[self.src_args.src_col_date])\
+                .groupby([self.src_args.src_col_isin])[self.trg_args.trg_col_op_price]\
+                    .shift(1)
+        data_frame[self.trg_args.trg_col_ch_prev_clos] = (
+            data_frame[self.trg_args.trg_col_op_price] \
+                - data_frame[self.trg_args.trg_col_ch_prev_clos]
+            ) / data_frame[self.trg_args.trg_col_ch_prev_clos] * 100
+        # Rounding to 2 decimals
+        data_frame = data_frame.round(decimals=2)
+        # Removing the day before extract_date
+        data_frame = data_frame[data_frame.Date >= self.extract_date].reset_index(drop=True)
+        self._logger.info('Appling transformations to Xetra source data finished...')
+        return data_frame
+
+    def load(self, data_frame:pd.DataFrame):
+        """
+        Saves a Pandas DataFram to the target
+
+        :param data_frame: Pandas DataFrame as Input
+        """
+        # Creating target key
+        target_key = (
+            f'{self.trg_args.trg_key}'
+            f'{datetime.today().strftime(self.trg_args.trg_key_date_format)}'
+            f'{self.trg_args.trg_format}'
+        )
+        # Writing to target
+        self.s3_bucket_trg.write_df_to_s3(data_frame, target_key, self.trg_args.trg_format)
+        self._logger.info('Xetra target data successfully written.')
+        # Updating meta file
+        MetaProcess.update_meta_file(self.meta_update_list, self.meta_key, self.s3_bucket_trg)
+        self._logger.info('Xetra meta file successfully updated.')
+        return True
 
     def etl_report1(self):
-        pass
+        """
+        Extract, transform and load to create report 1
+        """
+        # Extraction
+        data_frame = self.extract()
+        # Transformation
+        data_frame = self.transform_report1(data_frame)
+        # Load
+        self.load(data_frame)
+        return True
